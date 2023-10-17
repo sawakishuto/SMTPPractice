a696ae26556c646d59bca79a6f5bfcf5a81762f6
Sawaki Shuto <swkshuto0208@icloud.com>
2023-10-17 11:52:23 +0900

    first commit

---
+++ SMTPprac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SMTPprac
+//
+//  Created by 澤木柊斗 on 2023/10/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SMTPprac/SMTPpracApp.swift
@@ -0,0 +1,17 @@
+//
+//  SMTPpracApp.swift
+//  SMTPprac
+//
+//  Created by 澤木柊斗 on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct SMTPpracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
