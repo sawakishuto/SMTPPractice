f0cb839f150af4f75fe0957e6715263dc8738026
Sawaki Shuto <swkshuto0208@icloud.com>
2023-10-17 13:00:43 +0900

    報告機能(メール)追加

--- SMTPprac/ContentView.swift
+++ SMTPprac/ContentView.swift
@@ -1,24 +1,74 @@
-//
-//  ContentView.swift
-//  SMTPprac
-//
-//  Created by 澤木柊斗 on 2023/10/17.
-//
-
+import MessageUI
 import SwiftUI
+import UIKit
+
+struct MailView: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentation
+
+    var address: [String]
+
+    var subject: String
+
+    var messageBody: String
+
+    class Coordinator: NSObject, MFMailComposeViewControllerDelegate {
+        @Binding var presentation: PresentationMode
+
+        init(
+            presentation: Binding<PresentationMode>
+        ) {
+            _presentation = presentation
+        }
+
+        func mailComposeController(
+            _ controller: MFMailComposeViewController,
+            didFinishWith result: MFMailComposeResult,
+            error: Error?
+        ) {
+            $presentation.wrappedValue.dismiss()
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(presentation: presentation)
+    }
+
+    func makeUIViewController(
+        context: UIViewControllerRepresentableContext<MailView>
+    ) -> MFMailComposeViewController {
+        let viewController = MFMailComposeViewController()
+        viewController.mailComposeDelegate = context.coordinator
+        viewController.setToRecipients(address)
+        viewController.setSubject(subject)
+        viewController.setMessageBody(messageBody, isHTML: false)
+        return viewController
+    }
+
+    func updateUIViewController(_ uiViewController: MFMailComposeViewController, context: UIViewControllerRepresentableContext<MailView>) {}
+}
 
 struct ContentView: View {
+    @State var isShowMailView = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button {
+            #if DEBUG
+            // シュミレータでは表示できない
+            isShowMailView = true
+            #endif
+        } label: {
+            Text("メール")
         }
-        .padding()
+        .sheet(isPresented: $isShowMailView) {
+            MailView(
+                address: ["swkshuto0208@icloud.com"],
+                subject: "報告",
+                messageBody: "サンプルアプリです"
+            )
+            .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(isShowMailView: false)
 }
